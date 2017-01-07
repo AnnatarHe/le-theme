768a9137d2e5a4bf0c71fd72dc6e96a490f04c49
AnnatarHe <iamhele1994@gmail.com>
2017-01-07 17:39:08 +0800

    remove rollup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles/index.styl'
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            hello
+        </div>
+    )
+}
+
+export default Home
+
